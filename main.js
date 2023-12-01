32596dda01b5fa6161fca9cea43d55eafccab28e
henrick03 <145169400+henrick03@users.noreply.github.com>
2023-11-30 22:36:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
 }
 
 
@@ -10,10 +11,23 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 //enquanto
-while (contador<9) {
-    listaDeTeclas[contador].onclik = tocaSomPom;
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas [contadoor];
+ const instrumento = listaDeTeclas[contador].classList[1];
+ //template string
+ const idAudio = `#som_${instrumento}`;
+
+ console.log(idAudio);
+
+
+    tecla.onclik = function () {
+        tocaSom('#som_tecla_pom');
+    }
+
 
     contador = contador + 1;
-    console.logo(contador);
+
+    console.log(contador);
 
 }
